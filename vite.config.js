e961957e2b2be9e6a7544cf26a735f45b6a1697c
Kevin <lepitenkevin@gmail.com>
2026-04-07 16:50:14 +0800

    delete pwa

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // NEW
-import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(), // NEW
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-able-icon.png'],
-      manifest: {
-        name: 'Gas Network Bogo',
-        short_name: 'GasNetwork',
-        description: 'Real-time fuel prices for Bogo City',
-        theme_color: '#2563eb', // Your blue theme
-        background_color: '#ffffff',
-        display: 'standalone', // This hides the browser address bar
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      }
-    })
   ],
 })
